8190972c2f478bfd032061d8fbf0ad8f10240613
Sofía Fernández <sofia.m.fernandez36@gmail.com>
2021-06-15 17:40:08 -0300

    Se creó y modificó el proyecto Criptomonedas

---
+++ src/components/Formulario.jsx
@@ -0,0 +1,101 @@
+import React, { useEffect, useState} from 'react';
+import styled from '@emotion/styled';
+import Error from './Error';
+
+import useMoneda from '../hooks/useMoneda';
+import useCriptomoneda from '../hooks/useCriptomoneda';
+import axios from 'axios';
+import PropTypes from 'prop-types';
+
+
+const Boton = styled.input  `
+    margin-top: 20px;
+    font-weight: bold;
+    font-size: 20px;
+    padding: 10px;
+    background-color: #66a2fe;
+    border: none;
+    width: 100%;
+    border-radius: 10px;
+    color:#fff;
+    transition: background-color .3s ease;
+
+    &:hover {
+        background-color: #326ac0;
+        cursor:pointer;
+    }
+`;
+
+const Formulario = ({guardarMoneda, guardarCriptomoneda}) => {
+    //state del listado de criptomonedas
+    const [ listacripto, guardarCriptomonedas] = useState([]);
+
+    //validar formulario
+    const [ error, guardarError] = useState(false);
+
+    const MONEDAS =  [
+        { codigo: 'USD', nombre: 'Dolar de EE.UU.'},
+        { codigo: 'ARS', nombre: 'Peso argentino'},
+        { codigo: 'MXN', nombre: 'Peso mexicano'},
+        { codigo: 'EUR', nombre: 'Euro'},
+        { codigo: 'GBP', nombre: 'Libra esterlina'}
+    ];
+    
+    //Utilizar useMoneda 
+    const [moneda, SelectMonedas ] = useMoneda('Elige tu moneda', '', MONEDAS);
+
+    //Utilizar useCriptomonedas
+
+    const [criptomoneda, SelectCripto] = useCriptomoneda('Elige tu criptomoneda','',listacripto);
+
+    //Ejecutar llamado a la API. Siempre useEffect es una función de flecha. 
+
+    useEffect(() =>{
+        const consultarAPI = async () => {
+            const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD';
+
+            const resultado = await axios.get(url);
+
+            guardarCriptomonedas(resultado.data.Data);
+        }
+        consultarAPI();
+    }, []);
+    //cuando el usuario hace submit
+
+    const cotizarMoneda = e => {
+        e.preventDefault();
+
+        //validar si ambos campos están llenos
+        if(moneda === '' || criptomoneda === '') {
+            guardarError(true);
+            return;
+        }
+
+        //pasar los datos al componente principal
+        guardarError(false);
+        guardarMoneda(moneda);
+        guardarCriptomoneda(criptomoneda);
+    }
+    return ( 
+        
+        <form 
+            onSubmit={cotizarMoneda}
+        >
+            {error ? <Error mensaje= "Todos los campos son obligatorios" />: null}
+
+            <SelectMonedas />
+            <SelectCripto />
+            <Boton 
+                type="submit"
+                value="Calcular"
+            />
+        </form>
+     );
+}
+
+Formulario.propTypes = {
+    guardarMoneda: PropTypes.func.isRequired,
+    guardarCriptomoneda: PropTypes.func.isRequired
+}
+ 
+export default Formulario;
